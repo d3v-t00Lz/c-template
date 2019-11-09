4c519d1ef5fa202bad09d5ffb9a7f70fa0c487ce
j4xnh0l3 <j4xnh0l3@nospam4me.org>
2019-11-08 20:05:36 -0800

    Add forking script

---
+++ fork.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+
+
+def _(x):
+    print(x)
+    retcode = os.system(x)
+    assert not retcode, retcode
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Forks the project into a new project."
+    )
+    parser.add_argument(
+        'name',
+        help='The new project name',
+    )
+    return parser.parse_args()
+
+def main():
+    file_path = os.path.abspath(__file__)
+    dirname = os.path.dirname(file_path)
+    os.chdir(dirname)
+
+    args = parse_args()
+    name = args.name
+    assert name
+    assert name.islower(), name
+    assert name.replace("_", "").isalpha(), name
+    assert len(name) < 20, len(name)
+
+    # Replace 'hello' with $name in files
+    _(
+        "find Makefile .gitignore "
+        "-type f "
+        "| xargs sed -i 's/hello/{name}/gI'".format(name=name)
+    )
+    #_('mv hello.spec {}.spec'.format(name))
+    _('rm -rf .git')
+    _('git init .')
+    _('git add .')
+    _('git commit -am "Initial commit"')
+
+    print("Successfully forked.")
+
+
+if __name__ == "__main__":
+    main()
