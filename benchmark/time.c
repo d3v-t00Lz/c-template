8b5c13822f45a68c5c99f3385b55fc851d838046
jaelen <jaelen@nospam4me.org>
2019-11-29 08:34:16 -0800

    Saner reporting of per-iteration performance

--- benchmark/time.c
+++ benchmark/time.c
@@ -7,22 +7,41 @@ void TimeFunc(
     char* name,
     size_t iterations
 ){
+    int i;
     clock_t start, end;
-    double time_used, seconds_per_iteration;
+    double time_used, units_per_iteration;
+    char* time_unit = NULL;
+    char time_units[5][20] = {
+        "seconds",
+        "milliseconds",
+        "microseconds",
+        "nanoseconds",
+        "picoseconds"
+    };
 
     start = clock();
     func();
     end = clock();
     time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    seconds_per_iteration = time_used / (double)(iterations);
+    units_per_iteration = (time_used / (double)(iterations));
+
+    for(i = 0; i < 5; ++i){
+        if(units_per_iteration >= 1.){
+            time_unit = time_units[i];
+            break;
+        }
+        units_per_iteration *= 1000.;
+    }
+
     printf(
 	"Ran %s:\n"
         "  %lu iterations\n"
         "  %f seconds\n"
-        "  %f seconds per iteration\n",
+        "  %f %s average per iteration\n",
 	name,
         iterations,
 	time_used,
-        seconds_per_iteration
+        units_per_iteration,
+        time_unit
     );
 }
